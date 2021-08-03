787a29a80c52be24c417d5ba6be8fcebf27e8c03
Nikola Stanojkovski <nikolaStanojkovski@users.noreply.github.com>
2021-08-03 18:55:57 +0200

    Initial Commit

---
+++ Implementation/src/main/java/pptech/skitproject/alibabatesting/AlibabaTestingApplication.java
@@ -0,0 +1,13 @@
+package pptech.skitproject.alibabatesting;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AlibabaTestingApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(AlibabaTestingApplication.class, args);
+    }
+
+}
---
+++ Implementation/src/test/java/pptech/skitproject/alibabatesting/AlibabaTestingApplicationTests.java
@@ -0,0 +1,13 @@
+package pptech.skitproject.alibabatesting;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AlibabaTestingApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
